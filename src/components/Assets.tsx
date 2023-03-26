c8e0022ac9cc20f2fe93545c4a289f32c7bbcc15
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-03-26 21:01:49 +0530

    Add Icons to AssetCard (#3)

--- src/components/Assets.tsx
+++ src/components/Assets.tsx
@@ -3,7 +3,7 @@ import AssetCard from "./AssetCard";
 
 const Assets: React.FC = () => {
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex items-center justify-between gap-4">
       {trendingAssets.map((asset) => (
         <AssetCard key={asset.name} asset={asset} />
       ))}
