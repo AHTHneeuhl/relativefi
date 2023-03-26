2f1723f9bbc014093175c799ac3fdb7aaf5c9881
Shubham Patrick <ahthneeuhl@gmail.com>
2023-03-26 13:08:01 +0530

    Create Typography & Header components

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import Typography from "./Typography";
+import activityIcon from "../assets/activity.svg";
+
+const Header: React.FC = () => {
+  return (
+    <div className="flex gap-4 mx-auto w-[90%] mt-32">
+      <img src={activityIcon} alt="activity" />
+      <Typography variant="h1" className="text-typography font-bold">
+        Trending Assets
+      </Typography>
+    </div>
+  );
+};
+
+export default Header;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      background: "#14172B",
+      typography: "#ECF0FF",
+      grey: "#5A5F7D",
+      "gradient-dark":
+        "linear-gradient(180deg, rgba(98, 106, 136, 0.1) 0%, rgba(46, 49, 73, 0.1) 107.43%)",
+      "gradient-light":
+        "linear-gradient(180deg, rgba(236, 240, 255, 0.1) 0%, rgba(46, 49, 73, 0) 107.43%)",
+    },
   },
   plugins: [],
 };
